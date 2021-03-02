12d29122f07c61f34e4b783bd8dd7061f33a74fb
Ida <so.kida@hotmail.com>
2021-03-01 19:15:13 -0500

    link done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import countries from './countries.json';
+import { Switch, Route, Link } from 'react-router-dom'
 
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
 
 function App() {
   return (
     <div className="App">
-    <Navbar/>
-    
-
+      <Navbar />
+      <div class="container">
+        <div class="row">
+          <CountriesList listOfCountries={countries} />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,11 @@ class Navbar extends React.Component {
 
 render(){
     return (
-        
+        <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+          <a className="navbar-brand" href="/">WikiCountries</a>
+        </div>
+      </nav>
     )
 }
 
