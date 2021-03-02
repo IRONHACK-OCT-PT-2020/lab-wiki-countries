0da5de45fcbb2189a44b52556cc2011a4ee62d92
RFeldman84 <rachel.feldman84@gmail.com>
2021-03-01 20:30:49 -0500

    country

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
-
+import countries from '../countries.json';
 // use{...} for
 
 class CountryDetails extends React.Component {
+  state = {
+    country: {},
+  };
+
   componentWillReceiveProps(props) {
     console.log(props);
     let cca3 = props.match.params.cca3;
     console.log(cca3);
+    let country = countries.find((eachCountry) => eachCountry.cca3 === cca3);
+    console.log(country);
+
+    this.setState({
+      country,
+    });
   }
+
   render() {
     return (
       <div className="col-7">
-        <h1>France</h1>
+        <h1>{this.state.country.name?.common}</h1>
         <table className="table">
           <thead></thead>
           <tbody>
             <tr>
               <td style={{ width: '30% ' }}>Capital</td>
-              <td>Paris</td>
+              <td>{this.state.country.capital}</td>
             </tr>
             <tr>
               <td>Area</td>
               <td>
-                551695 km
+                {this.state.country.area}km
                 <sup>2</sup>
               </td>
             </tr>
